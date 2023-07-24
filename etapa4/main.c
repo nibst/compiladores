adeb685c87a15353352702e9245ca36dd2f321b2
nikolas <nikolasps7@gmail.com>
2023-07-24 01:56:04 -0300

    version 1.0

--- etapa4/main.c
+++ etapa4/main.c
@@ -15,31 +15,22 @@ void initMe(void);
 
 int main(int argc, char **argv)
 {
-    FILE *gold = 0;
-    int token = 0;
-    int answar = 0;
-    int nota = 0;
     int i = 1;
-    fprintf(stderr, "Rodando main do prof. \n");
 
-    if (argc < 3)
+    if (argc < 2)
     {
-        printf("call: ./etapa3 input.txt out.txt\n");
+        printf("call: ./etapa4 input.txt\n");
         exit(1);
     }
     if (0 == (yyin = fopen(argv[1], "r")))
     {
         printf("Cannot open file %s... \n", argv[1]);
-        exit(1);
-    }
-    if (0 == (yyout = fopen(argv[2], "w")))
-    {
-        printf("Cannot open file %s... \n", argv[2]);
-        exit(1);
+        exit(2);
     }
     initMe();
     yyparse();
     fclose(yyout);
     
     fprintf(stderr,"compilation success\n");
+    return 0;
 }
--- etapa4/semantic.c
+++ etapa4/semantic.c
@@ -31,32 +31,9 @@ int getDataTypeOfTypeNode(AST *type_node){
     }
 }
 int isNumber(AST* operand_node){
-    return
-        (operand_node->type == AST_ADD || operand_node->type == AST_SUB ||
-        operand_node->type == AST_DIV || operand_node->type == AST_MUL ||
-        (operand_node->type == AST_SYMBOL && operand_node->symbol->type == SYMBOL_LIT_INT) ||
-        (operand_node->type == AST_SYMBOL && operand_node->symbol->type == SYMBOL_LIT_REAL) ||
-        (operand_node->type == AST_SYMBOL && operand_node->symbol->type == SYMBOL_LIT_CHAR) ||
-        (operand_node->type == AST_SYMBOL && 
-            (operand_node->symbol->type == SYMBOL_VARIABLE) && (
-                (operand_node->symbol->datatype == DATATYPE_INT) || 
-                (operand_node->symbol->datatype == DATATYPE_REAL) ||
-                (operand_node->symbol->datatype == DATATYPE_CHAR)
-            )
-        ) ||
-        (operand_node->type == AST_FUNC_CALL && (
-            (operand_node->sons[0]->symbol->datatype == DATATYPE_INT) || 
-            (operand_node->sons[0]->symbol->datatype == DATATYPE_REAL) ||
-            (operand_node->sons[0]->symbol->datatype == DATATYPE_CHAR)
-            )
-        )||
-        (operand_node->type == AST_VEC_IND && (
-            (operand_node->sons[0]->symbol->datatype == DATATYPE_INT) || 
-            (operand_node->sons[0]->symbol->datatype == DATATYPE_REAL) ||
-            (operand_node->sons[0]->symbol->datatype == DATATYPE_CHAR)
-            )
-        )
-    );
+    return (operand_node->datatype == DATATYPE_CHAR || 
+            operand_node->datatype == DATATYPE_INT  || 
+            operand_node->datatype == DATATYPE_REAL);
 }
 void setSymbolDataType(AST *symbol_node){
     //if hash datatype not setted, then it is a literal
@@ -74,6 +51,7 @@ void setSymbolDataType(AST *symbol_node){
         case SYMBOL_LIT_STRING:
             symbol_node->datatype = DATATYPE_STRING;
         default:
+            symbol_node->datatype = DATATYPE_NA;
             break;
         }
     }
@@ -99,13 +77,17 @@ void checkFunctionArguments(AST* func_dec, AST* current_node){
                 ++parameters_length;
                 if(args_node){
                     if(!isCompatibleDataType(parameters_node->sons[1]->datatype, current_node->sons[0]->datatype)){
-                        fprintf(stderr,"Semantic ERROR: argument %d of function call %s is not compatible type to function parameter\n",arguments_length,func_dec->sons[1]->symbol->text);
+                        fprintf(stderr,"Semantic ERROR: argument %d of function call '%s' is not compatible type to function parameter\n",parameters_length,func_dec->sons[1]->symbol->text);
+                        ++SemanticErrors;
                     }
                     args_node=args_node->sons[1];
                 }
             }
-            if(parameters_length != arguments_length)
-                fprintf(stderr,"Semantic ERROR: mismatch of arguments amout in function call %s\n",func_dec->sons[1]->symbol->text);
+            if(parameters_length != arguments_length){
+                fprintf(stderr,"Semantic ERROR: mismatch of arguments amount in function call '%s'\n",func_dec->sons[1]->symbol->text);
+                ++SemanticErrors;
+            }
+                
         }
     }
 
@@ -125,8 +107,8 @@ void checkFunction(AST* top_node,AST *node){
 /*-----------------------------------------------------semantic.h functions--------------------------------------0-----------------------*/
 void semanticAnalysis(AST* node){
     checkAndSetDeclaration(node);
-    setAstExpressionsDatatype(node);
     checkUndeclared();
+    setAstExpressionsDatatype(node);
     checkNature(node);
     checkFunctionParameterVersusArguments(node);
     checkOperands(node);
@@ -142,11 +124,17 @@ void checkVectorInitialization(AST *node){
             type = vector_ini->sons[0]->symbol->type;
         else{
             if(type == SYMBOL_LIT_INT || type == SYMBOL_LIT_CHAR)
-                if(vector_ini->sons[0]->symbol->type != SYMBOL_LIT_INT && vector_ini->sons[0]->symbol->type != SYMBOL_LIT_CHAR)
+                if(vector_ini->sons[0]->symbol->type != SYMBOL_LIT_INT && vector_ini->sons[0]->symbol->type != SYMBOL_LIT_CHAR){
                     fprintf(stderr,"Semantic ERROR: vector '%s' is not homogeneous\n",node->sons[1]->symbol->text);
+                    ++SemanticErrors;
+                }
+                    
             if(type == SYMBOL_LIT_REAL)
-                if(vector_ini->sons[0]->symbol->type != SYMBOL_LIT_REAL)
+                if(vector_ini->sons[0]->symbol->type != SYMBOL_LIT_REAL){
                     fprintf(stderr,"Semantic ERROR: vector '%s' is not homogeneous\n",node->sons[1]->symbol->text);
+                    ++SemanticErrors;
+                }
+                    
         }
     }
     if (length > atoi(node->sons[2]->symbol->text)){
@@ -163,6 +151,7 @@ void checkFunctionReturnType(DataType expected_return_type, AST *node){
         setAstExpressionsDatatype(node);
         if(!isCompatibleDataType(expected_return_type,node->sons[0]->datatype)){
             fprintf(stderr,"Semantic ERROR: return type different from declared type of function\n");
+            ++SemanticErrors;
         }
     }
     for (int i = 0; i<MAX_SONS; ++i)
@@ -186,7 +175,7 @@ void checkAndSetDeclaration(AST* node){
                 if(symbol->datatype == DATATYPE_INT || symbol->datatype == DATATYPE_CHAR)
                     if (!(node->sons[2]->symbol->type == SYMBOL_LIT_INT || node->sons[2]->symbol->type == SYMBOL_LIT_CHAR)){
                         fprintf(stderr,"Semantic ERROR: variable '%s' has different type than its initialization\n",symbol->text);
-                        
+                        ++SemanticErrors;
                     }      
                 if(symbol->datatype == DATATYPE_REAL)
                     if (!(node->sons[2]->symbol->type  == SYMBOL_LIT_REAL)){
@@ -205,7 +194,6 @@ void checkAndSetDeclaration(AST* node){
                 }
                 symbol->type = SYMBOL_FUNCTION;
                 symbol->datatype = getDataTypeOfTypeNode(node->sons[0]);
-                checkFunctionReturnType(symbol->datatype,node);
                 //check for parameters declared on function 
                 for(AST* parameters_node = node->sons[2]; parameters_node; parameters_node = parameters_node->sons[2]){
                     symbol = parameters_node->sons[1]->symbol;
@@ -218,6 +206,8 @@ void checkAndSetDeclaration(AST* node){
                         symbol->datatype = getDataTypeOfTypeNode(parameters_node->sons[0]);
                     }   
                 }
+                symbol = node->sons[1]->symbol;
+                checkFunctionReturnType(symbol->datatype,node);
             }
             break;
         case AST_VEC_DEC:
@@ -249,11 +239,13 @@ void checkOperands(AST *node){
             if(node->sons[0]){
                 if(!isNumber(node->sons[0])){
                     fprintf(stderr,"Semantic ERROR: invalid left operand for ADD\n");
+                    ++SemanticErrors;
                 }
             }
             if(node->sons[1]){
                 if(!isNumber(node->sons[1])){
                     fprintf(stderr,"Semantic ERROR: invalid right operand for ADD\n");
+                    ++SemanticErrors;
                 }
             }
             break;
@@ -261,11 +253,13 @@ void checkOperands(AST *node){
             if(node->sons[0]){
                 if(!isNumber(node->sons[0])){
                     fprintf(stderr,"Semantic ERROR: invalid left operand for SUB\n");
+                    ++SemanticErrors;
                 }
             }
             if(node->sons[1]){
                 if(!isNumber(node->sons[1])){
                     fprintf(stderr,"Semantic ERROR: invalid right operand for SUB\n");
+                    ++SemanticErrors;
                 }
             }
             
@@ -274,11 +268,13 @@ void checkOperands(AST *node){
             if(node->sons[0]){
                 if(!isNumber(node->sons[0])){
                     fprintf(stderr,"Semantic ERROR: invalid left operand for MUL\n");
+                    ++SemanticErrors;
                 }
             }
             if(node->sons[1]){
                 if(!isNumber(node->sons[1])){
                     fprintf(stderr,"Semantic ERROR: invalid right operand for MUL\n");
+                    ++SemanticErrors;
                 }
             }
             
@@ -287,11 +283,13 @@ void checkOperands(AST *node){
             if(node->sons[0]){
                 if(!isNumber(node->sons[0])){
                     fprintf(stderr,"Semantic ERROR: invalid left operand for DIV\n");
+                    ++SemanticErrors;
                 }
             }
             if(node->sons[1]){
                 if(!isNumber(node->sons[1])){
                     fprintf(stderr,"Semantic ERROR: invalid right operand for DIV\n");
+                    ++SemanticErrors;
                 }
             }
             
@@ -304,25 +302,53 @@ void checkOperands(AST *node){
         case AST_EQUAL:
             if(node->sons[0]){
                 if(!isNumber(node->sons[0])){
-                    fprintf(stderr,"Semantic ERROR: invalid left operand for binary logic operation\n");
+                    fprintf(stderr,"Semantic ERROR: invalid left operand for binary LOGIC OPERATION\n");
+                    ++SemanticErrors;
                 }
             }
             if(node->sons[1]){
                 if(!isNumber(node->sons[1])){
-                    fprintf(stderr,"Semantic ERROR: invalid right operand for binary logic operation\n");
+                    fprintf(stderr,"Semantic ERROR: invalid right operand for binary LOGIC OPERATION\n");
+                    ++SemanticErrors;
                 }
             }
-            
+            break;
+        case AST_AND:
+        case AST_OR:
+            if(node->sons[0]){
+                if (!(node->sons[0]->datatype == DATATYPE_BOOL)){
+                        fprintf(stderr,"Semantic ERROR: invalid left operand for binary LOGIC OPERATION\n");
+                        ++SemanticErrors;
+                    }
+            }
+            if(node->sons[1]){
+                if (!(node->sons[1]->datatype == DATATYPE_BOOL)){
+                        fprintf(stderr,"Semantic ERROR: invalid right operand for binary LOGIC OPERATION\n");
+                        ++SemanticErrors;
+                    }
+            }
+            break;
+
+        case AST_NOT:
+            if(node->sons[0]){
+                if (!(node->sons[0]->datatype == DATATYPE_BOOL)){
+                        fprintf(stderr,"Semantic ERROR: invalid left operand for unary LOGIC OPERATION\n");
+                        ++SemanticErrors;
+                    }
+            }
             break;
         case AST_ATTRIB:
             //check datatype of left side attrib, check datatype of right AST_node side of attrib, if both are equal or equivalent type(char or int) than its ok
             if (!isCompatibleDataType(node->sons[0]->datatype,node->sons[1]->datatype)){
                 fprintf(stderr,"Semantic ERROR: ATTRIB not compatible types\n");
+                ++SemanticErrors;
             }
             break;
         case AST_VEC_IND:
-            if(!isCompatibleDataType(DATATYPE_INT,node->sons[1]->datatype))
+            if(!isCompatibleDataType(DATATYPE_INT,node->sons[1]->datatype)){
                 fprintf(stderr,"Semantic ERROR: can't index vector with another type other than integer or compatible type\n");
+                ++SemanticErrors;
+            }
             break;
         default:
             break;
@@ -345,14 +371,22 @@ void setAstExpressionsDatatype(AST *expr_node){
         case AST_MUL: 
         case AST_DIV: 
             if(isCompatibleDataType(expr_node->sons[0]->datatype,expr_node->sons[1]->datatype)){
-                if (expr_node->sons[0]->datatype == DATATYPE_CHAR || expr_node->sons[1]->datatype == DATATYPE_CHAR)
+                if (expr_node->sons[0]->datatype == DATATYPE_CHAR || expr_node->sons[0]->datatype == DATATYPE_INT)
                     expr_node->datatype = DATATYPE_INT; //arithmetic op between chars or char+int is int
-                else
-                    //else so just choose first one (both datatypes are equal in this case because the only compatibility is between char and int)
-                    expr_node->datatype = expr_node->sons[0]->datatype; 
+                //else both datatypes are equal because the only compatibility is between char and int
+                else{
+                    //if operands are real, then the result is real
+                    if (expr_node->sons[0]->datatype == DATATYPE_REAL)
+                        expr_node->datatype = DATATYPE_REAL; 
+                    //if operand is not char,int or real, then the result is not applicable
+                    else expr_node->datatype = DATATYPE_NA; 
+                }
             }
-            else
-                fprintf(stderr,"Semantic ERROR: not compatible types of arithmetic expression\n");
+            else{
+                expr_node->datatype = DATATYPE_NA; 
+                fprintf(stderr,"Semantic ERROR: not compatible types in ARITHMETIC OPERATION\n");
+                ++SemanticErrors;
+            }
             break;
         case AST_GREATER: 
         case AST_GREATER_EQUAL: 
@@ -361,10 +395,33 @@ void setAstExpressionsDatatype(AST *expr_node){
         case AST_DIFFERENT: 
         case AST_EQUAL:
             if(isCompatibleDataType(expr_node->sons[0]->datatype,expr_node->sons[1]->datatype)){
-                expr_node->datatype = DATATYPE_BOOL; //boolean op always result in bool
+                if(expr_node->sons[0]->datatype == DATATYPE_INT || expr_node->sons[0]->datatype == DATATYPE_CHAR || expr_node->sons[0]->datatype == DATATYPE_REAL)
+                    expr_node->datatype = DATATYPE_BOOL; //comparsion operation result in bool if operands are numbers
+                else expr_node->datatype = DATATYPE_NA; //if operands are not numbers, result in not aplicable(NA)
             }
-            else
+            else{
+                expr_node->datatype = DATATYPE_NA;
+                fprintf(stderr,"Semantic ERROR: not compatible types on COMPARSION OPERATION\n");
+                ++SemanticErrors;
+            }
+            break;
+        case AST_AND:
+        case AST_OR:
+            if(isCompatibleDataType(expr_node->sons[0]->datatype,expr_node->sons[1]->datatype)){
+                if(expr_node->sons[0]->datatype == DATATYPE_BOOL)
+                    expr_node->datatype = DATATYPE_BOOL; //logic op result in bool if operands are booleans
+                else expr_node->datatype = DATATYPE_NA; //if operands are not booleans, result in not aplicable(NA)
+            }
+            else{
+                expr_node->datatype = DATATYPE_NA;
                 fprintf(stderr,"Semantic ERROR: not compatible types on LOGIC OPERATION\n");
+                ++SemanticErrors;
+            }
+            break;
+        case AST_NOT:
+            if(expr_node->sons[0]->datatype == DATATYPE_BOOL)
+                expr_node->datatype = DATATYPE_BOOL; //logic op result in bool if operands are booleans
+            else expr_node->datatype = DATATYPE_NA; //if operands are not booleans, result in not aplicable(NA)
             break;
         case AST_VEC_IND:
             expr_node->datatype = expr_node->sons[0]->datatype;
@@ -401,13 +458,13 @@ void checkNature(AST *node){
         break;
     case AST_VEC_IND:
         if (node->sons[0]->symbol->type != SYMBOL_VECTOR){
-            fprintf(stderr,"Semantic ERROR: usage of vector is incorrect\n");
+            fprintf(stderr,"Semantic ERROR: usage of vector nature to a non vector natured identifier\n");
             ++SemanticErrors;
         }
         return; //dont check further down this tree
     case AST_FUNC_CALL:
         if (node->sons[0]->symbol->type != SYMBOL_FUNCTION){
-            fprintf(stderr,"Semantic ERROR:  usage of function is incorrect\n");
+            fprintf(stderr,"Semantic ERROR:  usage of function nature to a non function natured identifier\n");
             ++SemanticErrors;
         }
         else
@@ -415,7 +472,7 @@ void checkNature(AST *node){
         return; //dont check further down this tree
     case AST_SYMBOL:
         if (node->symbol->type == SYMBOL_FUNCTION ||  node->symbol->type == SYMBOL_VECTOR){
-            fprintf(stderr,"Semantic ERROR: usage of variable is incorrect\n");
+            fprintf(stderr,"Semantic ERROR: usage of variable nature to a non variable natured identifier\n");
             ++SemanticErrors;
         }
         break;
